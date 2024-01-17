9afb0a729bd06c8e480326701397cda0078825d1
Sebastian Minkowski <91944596+lemmgator@users.noreply.github.com>
2024-01-17 15:20:58 +0100

    cleanup

--- sc2/app/user/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class IsOwnerOrAdminPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return True
-
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user or request.user.is_staff
